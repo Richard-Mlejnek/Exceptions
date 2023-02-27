5099af3002f04784de9a383a0ac4cc0acce31e88
mlejnri20 <mlejnri20@sps-prosek.cz>
2023-02-27 12:51:30 +0100

    Program completed

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Exceptions;
+
+public class MyException : Exception
+{
+    public MyException() : base(message:"Pokazilo se to") {}
+}
+
+public static class Program
+{
+    static void Main()
+    {
+        int[] arr = new int[5];
+        int pos = 10;
+
+        try
+        {
+            arr[pos] = 5;
+        }
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine("Index pole je vetsi nez kapacita pole");
+        }
+
+        Console.WriteLine("Program dal bezi");
+        
+        try
+        {
+            if (pos >= arr.Length)
+            {
+                throw new MyException();
+            }
+        }
+        catch (MyException e)
+        {
+            Console.WriteLine(e);
+        }
+        
+        Console.WriteLine("Program nespadnul a skonci");
+    }
+}
